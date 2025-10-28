412da0b437cb6bb3acc214f0e92dd6a2c3e5f966
rory <35566455+Thorekt@users.noreply.github.com>
2025-10-28 13:55:23 +0100

    Test: ajout du test du service TeacherService

--- back/src/test/java/com/openclassrooms/starterjwt/services/TeacherServiceTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/services/TeacherServiceTest.java
@@ -1,5 +1,36 @@
 package com.openclassrooms.starterjwt.services;
 
+import org.junit.jupiter.api.Test;
+
+import com.openclassrooms.starterjwt.repository.TeacherRepository;
+
 public class TeacherServiceTest {
+
+    TeacherRepository mockTeacherRepository = org.mockito.Mockito.mock(TeacherRepository.class);
+
+    TeacherService classUnderTest = new TeacherService(mockTeacherRepository);
+
+    @Test
+    public void testFindAll() {
+        // Given
+
+        // When
+        classUnderTest.findAll();
+
+        // Then
+        org.mockito.Mockito.verify(mockTeacherRepository).findAll();
+    }
+
+    @Test
+    public void testFindById() {
+        // Given
+        Long teacherId = 1L;
+
+        // When
+        classUnderTest.findById(teacherId);
+
+        // Then
+        org.mockito.Mockito.verify(mockTeacherRepository).findById(teacherId);
+    }
 
 }
