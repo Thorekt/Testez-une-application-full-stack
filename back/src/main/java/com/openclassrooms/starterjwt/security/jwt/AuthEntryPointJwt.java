f07ade236ce2ef21f7aea5898e09bb20aedb17a6
rory <35566455+Thorekt@users.noreply.github.com>
2025-11-10 17:39:42 +0100

    revert: retrait d'un Bug inserer lors du dernier commit

--- back/src/main/java/com/openclassrooms/starterjwt/security/jwt/AuthEntryPointJwt.java
+++ back/src/main/java/com/openclassrooms/starterjwt/security/jwt/AuthEntryPointJwt.java
@@ -20,7 +20,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Component
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
 
-  private static final Logger logger = LoggerFactory.getLogger(AuthEntryPointJwtTest.class);
+  private static final Logger logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
 
   @Override
   public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
