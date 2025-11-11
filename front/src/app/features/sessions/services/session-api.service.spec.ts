b388c413a196c65a0e037801be6658a60c1ca3f3
rory <35566455+Thorekt@users.noreply.github.com>
2025-11-11 18:06:51 +0100

    Test(front): Test de toutes les méthodes du service SessionApiService

--- front/src/app/features/sessions/services/session-api.service.spec.ts
+++ front/src/app/features/sessions/services/session-api.service.spec.ts
@@ -19,4 +19,97 @@ describe('SessionsService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should all call HttpClient get method', () => {
+    // Given
+    const httpClientSpy = jest.spyOn(service['httpClient'], 'get').mockReturnValueOnce({} as any);
+
+    // When
+    const observable = service.all();
+
+    // Then
+    expect(httpClientSpy).toHaveBeenCalled();
+    expect(observable).toBeDefined();
+
+  });
+
+  it('should detail call HttpClient get method with id in url', () => {
+    // Given
+    const httpClientSpy = jest.spyOn(service['httpClient'], 'get').mockReturnValueOnce({} as any);
+    const id = '123';
+
+    // When
+    const observable = service.detail(id);
+
+    // Then
+    expect(httpClientSpy).toHaveBeenCalledWith(`api/session/${id}`);
+    expect(observable).toBeDefined();
+  });
+
+  it('should delete call HttpClient delete method with id in url', () => {
+    // Given
+    const httpClientSpy = jest.spyOn(service['httpClient'], 'delete').mockReturnValueOnce({} as any);
+    const id = '123';
+
+    // When
+    const observable = service.delete(id);
+
+    // Then
+    expect(httpClientSpy).toHaveBeenCalledWith(`api/session/${id}`);
+    expect(observable).toBeDefined();
+  });
+
+  it('should create call HttpClient post method', () => {
+    // Given
+    const httpClientSpy = jest.spyOn(service['httpClient'], 'post').mockReturnValueOnce({} as any);
+    const session = { } as any;
+
+    // When
+    const observable = service.create(session);
+
+    // Then
+    expect(httpClientSpy).toHaveBeenCalledWith('api/session', session);
+    expect(observable).toBeDefined(); 
+  });
+
+  it('should update call HttpClient put method with id in url', () => {
+    // Given
+    const httpClientSpy = jest.spyOn(service['httpClient'], 'put').mockReturnValueOnce({} as any);
+    const id = '123';
+
+    // When
+    const observable = service.update(id, {} as any);
+
+    // Then
+    expect(httpClientSpy).toHaveBeenCalledWith(`api/session/${id}`, {});
+    expect(observable).toBeDefined();
+  });
+
+  it('should participate call HttpClient post method with id and userId in url', () => {
+    // Given
+    const httpClientSpy = jest.spyOn(service['httpClient'], 'post').mockReturnValueOnce({} as any);
+    const id = '123';
+    const userId = '456';
+
+    // When
+    const observable = service.participate(id, userId);
+
+    // Then
+    expect(httpClientSpy).toHaveBeenCalledWith(`api/session/${id}/participate/${userId}`, null);
+    expect(observable).toBeDefined();
+  });
+
+  it('should unParticipate call HttpClient delete method with id and userId in url', () => {
+    // Given
+    const httpClientSpy = jest.spyOn(service['httpClient'], 'delete').mockReturnValueOnce({} as any);
+    const id = '123';
+    const userId = '456';
+
+    // When
+    const observable = service.unParticipate(id, userId);
+
+    // Then
+    expect(httpClientSpy).toHaveBeenCalledWith(`api/session/${id}/participate/${userId}`);
+    expect(observable).toBeDefined();
+  });
 });
