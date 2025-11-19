0fe279897375766855d214f630db6d0e213f6246
rory <35566455+Thorekt@users.noreply.github.com>
2025-11-19 17:05:28 +0100

    test: test d'intégrationd controller + service + mapper sur TeacherController

---
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/TeacherControllerSIT.java
@@ -0,0 +1,119 @@
+package com.openclassrooms.starterjwt.controllers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.openclassrooms.starterjwt.models.Teacher;
+import com.openclassrooms.starterjwt.repository.TeacherRepository;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class TeacherControllerSIT {
+
+        @Autowired
+        private MockMvc mockMvc;
+
+        @MockitoBean
+        private TeacherRepository teacherRepository;
+
+        @Test
+        @WithMockUser
+        void givenTeacherController_whenFindById_withValidId_thenReturnsTeacher() throws Exception {
+                // Given
+                Long id = 1L;
+                Teacher teacher = Teacher.builder()
+                                .id(id)
+                                .firstName("John")
+                                .lastName("Doe")
+                                .build();
+
+                when(teacherRepository.findById(id))
+                                .thenReturn(Optional.of(teacher));
+
+                // When
+                MvcResult result = mockMvc.perform(
+                                MockMvcRequestBuilders.get("/api/teacher/" + id)).andReturn();
+
+                // Then
+                MockHttpServletResponse response = result.getResponse();
+
+                assertEquals(200, response.getStatus());
+                assertTrue(response.getContentAsString().contains("John"));
+                assertTrue(response.getContentAsString().contains("Doe"));
+
+                verify(teacherRepository).findById(id);
+        }
+
+        @Test
+        @WithMockUser
+        void givenTeacherController_whenFindById_withInvalidId_thenReturnsNotFound() throws Exception {
+                // Given
+                Long id = 999L;
+
+                when(teacherRepository.findById(id))
+                                .thenReturn(Optional.empty());
+
+                // When
+                MvcResult result = mockMvc.perform(
+                                MockMvcRequestBuilders.get("/api/teacher/" + id))
+                                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                                .andReturn();
+
+                // Then
+                MockHttpServletResponse response = result.getResponse();
+
+                assertEquals(404, response.getStatus());
+                verify(teacherRepository).findById(id);
+        }
+
+        @Test
+        @WithMockUser
+        void givenTeacherController_whenFindById_withInvalidFormatId_thenReturnsBadRequest() throws Exception {
+                // Given
+                String invalidId = "abc";
+
+                // When
+                MvcResult result = mockMvc.perform(
+                                MockMvcRequestBuilders.get("/api/teacher/" + invalidId))
+                                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                                .andReturn();
+
+                // Then
+                MockHttpServletResponse response = result.getResponse();
+
+                assertEquals(400, response.getStatus());
+        }
+
+        @Test
+        void givenTeacherController_whenFindById_withNoUserLoggedIn_thenReturnsUnauthorized() throws Exception {
+                // Given
+                Long id = 1L;
+
+                // When
+                MvcResult result = mockMvc.perform(
+                                MockMvcRequestBuilders.get("/api/teacher/" + id))
+                                .andExpect(MockMvcResultMatchers.status().isUnauthorized())
+                                .andReturn();
+
+                // Then
+                MockHttpServletResponse response = result.getResponse();
+
+                assertEquals(401, response.getStatus());
+        }
+}
